76c1f47716477884d8f25565edbb87985a6deafa
Nugar <cladeadenugar@gmail.com>
2024-11-27 15:40:43 +0100

    completed signin user interface

--- app/components/Signin.tsx
+++ app/components/Signin.tsx
@@ -1,14 +1,30 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Signin = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [checked, setChecked] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
+  const [disabled, setDisabled] = useState(true);
+
+  useEffect(() => {
+    if (username !== "" && password !== "" && checked) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }, [username, password, checked]);
+
+  const handleSignIn = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
 
   return (
     <div className="h-screen flex flex-col justify-center items-center">
       <div className="space-y-4">
-        <div className="space-y-2">
+        <div className="space-y-2 text-center px-8">
           <h1 className="text-[#000000] font-bold text-2xl">Sign in</h1>
           <h1 className="text-[#4B5563] text-sm cursor-pointer">
             Don't have an account yet?{" "}
@@ -18,7 +34,7 @@ const Signin = () => {
           </h1>
         </div>
 
-        <form className="space-y-3">
+        <form onSubmit={handleSignIn} className="space-y-[0.875rem]">
           {/* username */}
           <div className="flex flex-col space-y-2">
             <label
@@ -71,6 +87,8 @@ const Signin = () => {
                 required
                 id="username"
                 name="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
                 className="border-none focus:outline-none w-full h-full placeholder:text-[#6B7280] text-[#000000] text-[0.938rem] font-medium"
               />
             </div>
@@ -121,6 +139,8 @@ const Signin = () => {
                 required
                 id="password"
                 name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 className="border-none focus:outline-none w-full h-full placeholder:text-[#6B7280] text-[#000000] text-[0.938rem] font-medium"
               />
 
@@ -135,30 +155,58 @@ const Signin = () => {
               >
                 <path
                   d="M6.58669 7.08667C6.39019 7.26977 6.23259 7.49057 6.12327 7.7359C6.01396 7.98123 5.95518 8.24607 5.95045 8.51461C5.94571 8.78315 5.99511 9.04989 6.0957 9.29893C6.19629 9.54796 6.346 9.77419 6.53592 9.96411C6.72584 10.154 6.95206 10.3037 7.2011 10.4043C7.45013 10.5049 7.71688 10.5543 7.98542 10.5496C8.25396 10.5448 8.51879 10.4861 8.76412 10.3768C9.00946 10.2674 9.23026 10.1098 9.41336 9.91334"
-                  stroke="#6B7280"
+                  stroke={showPassword ? "#2563EB" : "#6B7280"}
                   strokeLinecap="round"
                   strokeLinejoin="round"
                 />
                 <path
                   d="M7.15332 3.88668C7.43419 3.85168 7.71694 3.83387 7.99999 3.83334C12.6667 3.83334 14.6667 8.50001 14.6667 8.50001C14.3686 9.13809 13.9948 9.73797 13.5533 10.2867"
-                  stroke="#6B7280"
+                  stroke={showPassword ? "#2563EB" : "#6B7280"}
                   strokeLinecap="round"
                   strokeLinejoin="round"
                 />
                 <path
                   d="M4.40665 4.90668C3.08081 5.80976 2.01989 7.05018 1.33331 8.50001C1.33331 8.50001 3.33331 13.1667 7.99998 13.1667C9.27725 13.1701 10.5272 12.7967 11.5933 12.0933"
-                  stroke="#6B7280"
+                  stroke={showPassword ? "#2563EB" : "#6B7280"}
                   strokeLinecap="round"
                   strokeLinejoin="round"
                 />
                 <path
                   d="M1.33331 1.83334L14.6666 15.1667"
-                  stroke="#6B7280"
+                  stroke={showPassword ? "#2563EB" : "#6B7280"}
                   strokeLinecap="round"
                   strokeLinejoin="round"
                 />
               </svg>
             </div>
+          </div>
+
+          {/* Remember me checkbox and submit button */}
+          <div className="space-y-7">
+            <div className="flex items-center space-x-3">
+              <input
+                type="checkbox"
+                id="checkbox"
+                name="checkbox"
+                checked={checked}
+                onChange={(e) => setChecked(!checked)}
+                required
+              />
+              <label
+                htmlFor="checkbox"
+                className="text-[#1F2937] font-semibold text-sm"
+              >
+                Remember me
+              </label>
+            </div>
+
+            <button
+              type="submit"
+              disabled={disabled}
+              className="bg-primary disabled:bg-[#60A5FA] w-full h-full py-3 font-semibold text-[0.938rem] text-white rounded-lg"
+            >
+              Sign in
+            </button>
           </div>
         </form>
       </div>
