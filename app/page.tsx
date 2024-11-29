c0734e986b965ee645c119013e6d7ff225dbd137
Nugar <cladeadenugar@gmail.com>
2024-11-29 18:22:26 +0100

    added initial loading animation

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,18 @@ import Signin from "./components/Signin";
 import Navbar from "./components/Navbar";
 import { useEffect, useState } from "react";
 import User from "./components/User";
+import Image from "next/image";
+import { motion } from "framer-motion";
 
 const Page = () => {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean | null>(null);
+  const [pageLoaded, setpageLoaded] = useState(false);
 
   useEffect(() => {
+    setTimeout(() => {
+      setpageLoaded(true);
+    }, 2000);
+
     const storedLoginStatus = localStorage.getItem("isLoggedIn");
     if (storedLoginStatus === "true") {
       setIsLoggedIn(true);
@@ -22,18 +29,52 @@ const Page = () => {
   }
 
   return (
-    <div>
-      {isLoggedIn ? (
-        <div>
-          <User />
-        </div>
+    <>
+      {pageLoaded ? (
+        <motion.div
+          key="content"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.7 }}
+        >
+          {isLoggedIn ? (
+            <div>
+              <User />
+            </div>
+          ) : (
+            <div className="h-screen flex flex-col">
+              <Navbar />
+              <Signin setIsLoggedIn={setIsLoggedIn} />
+            </div>
+          )}
+        </motion.div>
       ) : (
-        <div className="h-screen flex flex-col">
-          <Navbar />
-          <Signin setIsLoggedIn={setIsLoggedIn} />
-        </div>
+        <motion.div
+          key="loading"
+          className="h-screen flex justify-center items-center"
+          initial={{ opacity: 1, scale: 1 }}
+          animate={{ opacity: 0, scale: 1.5 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 1.5, delay: 1 }}
+        >
+          <motion.div
+            initial={{ scale: 1, opacity: 1 }}
+            animate={{ scale: 1.5, opacity: 0 }}
+            transition={{ duration: 1.5, ease: "easeInOut", delay: 0.5 }}
+            className="w-[2rem] h-[2rem] relative"
+          >
+            <Image
+              fill
+              priority
+              src="/icon.png"
+              alt="icon"
+              className="object-cover"
+            />
+          </motion.div>
+        </motion.div>
       )}
-    </div>
+    </>
   );
 };
 
